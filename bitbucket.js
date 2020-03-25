78ca7d41f3b7234dad246be68dd2544f8190fa83
Neil Kumar <neil.kumar@eigentech.com>
2020-03-25 18:15:18 +0000

    add chrome extension

---
+++ bitbucket.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function copyBranchName(){
+  const branchNameEl = document.querySelector('.branch-name');
+  const range = document.createRange();
+  const selection = window.getSelection();
+
+  range.selectNode(branchNameEl);
+
+  selection.removeAllRanges();
+  selection.addRange(range);
+
+  document.execCommand('copy');
+
+  selection.removeAllRanges();
+}
+
+function isValidPage(){
+  const isBitbucket = window.location.hostname === "HOST_HERE";
+  const isAPr = window.location.href.match(/\/pull-requests\/(\d+)/);
+
+  return isBitbucket && isAPr;
+}
+
+function addCopyButton(){
+  const header = document.querySelector('#pull-request-header');
+  const button = document.createElement('button');
+  button.textContent = "Copy Branch Name";
+  button.onclick = copyBranchName;
+
+  header.prepend(button);
+}
+
+if(isValidPage()){
+  addCopyButton()
+}
